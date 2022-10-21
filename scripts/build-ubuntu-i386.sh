fb2f3e6fdcda598e415416809de3f484bbc6e540
zhang shan <magisk6666@gmail.com>
2022-10-21 22:37:04 +0800

    build Android hl2mp

--- scripts/build-ubuntu-i386.sh
+++ scripts/build-ubuntu-i386.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 git submodule init && git submodule update
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install -f -y libopenal-dev:i386 g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386 libedit-dev:i386
-
-PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ./waf configure -T debug --disable-warns $* &&
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
+unzip android-ndk-r10e-linux-x86_64.zip
+export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
+export NDK_HOME=$PWD/android-ndk-r10e/
+./waf --help
+./waf configure -T release --android=armeabi-v7a-hard,4.9,21 --togles --build-game=hl2mp --disable-warns &&
 ./waf build
+#git submodule init && git submodule update
+#sudo dpkg --add-architecture i386
+#sudo apt-get update
+#sudo apt-get install -f -y libopenal-dev:i386 g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386 libedit-dev:i386
+
+#PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ./waf configure -T debug --disable-warns $* &&
+#./waf build
